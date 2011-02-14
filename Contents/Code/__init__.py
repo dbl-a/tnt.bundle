d8ead69c8ff6aa804c93e31e9f24e6848d96b6fe
dbl-a <aajr@mac.com>
2011-02-14 09:33:50 -0600

    rewrite for XML & RTMP

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,129 +1,77 @@
 # PMS plugin framework
-import datetime,re
-
 
 ##################################################################################################TNT
-VIDEO_PREFIX     = "/video/TNT"
+VIDEO_PREFIX  = "/video/TNT"
 NAME          = L('Title')
 
-TNT_URL                     = "http://www.tnt.tv"
-TNT_FULL_EPISODES_SHOW_LIST = "http://www.tnt.tv/series"
+TNT_ROOT      = "http://www.tnt.tv/"
+SHOW_LIST     = "http://www.tnt.tv/content/services/getCollections.do?id=58127"
+AUTH_URL      = "http://www.tnt.tv/processors/services/token.do"
 
-TNT_FEED                    = "http://www.TNT.com/"
-DEBUG                       = False
-TNTart                      ="art-default.jpg"
-TNTthumb                    ="icon-default.jpg"
+ART           = "art-default.jpg"
+THUMB         = "icon-default.jpg"
 
 ####################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, 'TNT',TNTthumb,TNTart)
+  Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, 'TNT', THUMB, ART)
   Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
   Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
   
-  MediaContainer.art        =R(TNTart)
+  MediaContainer.art        = R(ART)
   MediaContainer.title1     = NAME
-  DirectoryItem.thumb       =R(TNTthumb)
+  DirectoryItem.thumb       = R(THUMB)
 
 ####################################################################################################
-#def VideoMainMenu():
-#    dir = MediaContainer(mediaType='video') 
-#    dir.Append(Function(DirectoryItem(all_shows, "All Shows"), pageUrl = TNT_FULL_EPISODES_SHOW_LIST))
-#    return dir
-    
-####################################################################################################
-def VideoMainMenu():
-    pageUrl=TNT_FULL_EPISODES_SHOW_LIST
+def MainMenu():
     dir = MediaContainer(viewGroup="List")
-    content1 = XML.ElementFromURL(pageUrl, isHTML="True")
-    showMap = dict()
-    shownum=0
-    for item in content1.xpath('//div[@id="navfullepisodes"]/table/tr'):
-        Log(item.findall('td/'))
-        things = item.findall('./td/a')
-        
-        for thingies in things:
-          shownum =shownum+1
-          showID=shownum
-          Log(thingies)
-          title=thingies.text
-          titleUrl=thingies.get('href')
-          titleUrl=TNT_URL + titleUrl
-          thumb=TNTthumb
-          Log(titleUrl)
-          Log(title)
-      #  Log(thumb)
-          showList = showMap.get(title)
-          if showList == None:
-		      showList = []
-		      showMap[title] = showList
-		  # Tuple order here matters
-          showList.append((showID,titleUrl, thumb, title))
-          Log(showList)
-    shows=showMap.keys()
-    Log(shows)
-    shows.sort()
-    Log(shows)
-    shownum=0
-    #for shownames in shows:
-    #  shownum=shownum+1
-    #  showList.append((shownum,shownames.titleUrl,shownames.thumb, shownames.title))
-    for showkey in shows:
-      Log(showkey)
-      for show in showMap[showkey]:
-        Log(show)
-        title=show[3]
-        url=show[1]
-        thumb=show[2]
-        Log("Show: " + title + " | link: " + url)
-        dir.Append(Function(DirectoryItem(showxml, title=title), pageUrl = url))
-      
-    
+    content = XML.ElementFromURL(SHOW_LIST)
+    for item in content.xpath('//collection[name="Full Episodes"]//subcollection'):
+        showId = item.xpath('./@id')[0]
+        title = item.xpath('.//name')[0].text
+        title = title.split(" -")[0]
+        dir.Append(Function(DirectoryItem(VideoPage, title=title), showId=showId))
     return dir 
 
 ####################################################################################################
-def showxml(sender, pageUrl):
-  dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", noCache=True)
-  showID=re.compile('cid=([0-9]+)').findall(pageUrl)[0]
-  link="http://www.tnt.tv/processors/services/getCollectionByContentId.do?offset=0&sort=&limit=200&id=" + showID
-  shows=XML.ElementFromURL(link).xpath('//episode')
+def VideoPage(sender, showId):
+  dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList")
+  link = "http://www.tnt.tv/processors/services/getCollectionByContentId.do?site=true&offset=0&sort=&limit=200&id=" + showId
+  shows = XML.ElementFromURL(link).xpath('//episode')
   for show in shows:
-    
-    #episodeID  
-    epID=show.xpath('./grouperId')[0].text
-    Log(epID)
-
-    #title
-    title=show.xpath('./title')[0].text
+    videoId = show.xpath('./@id')[0]
+    Log(videoId)
+    title = show.xpath('./title')[0].text
     Log(title)
-
-    #thumb
-    thumb=show.xpath('./thumbnailUrl')[0].text
-    #summary
-    summary=show.xpath('./description')[0].text
-    clip="http://www.tnt.tv/dramavision/index.jsp?oid=" + epID 
-    Log("link: " + clip)   
-    
-    
-##########   This section knows the location of the flv file, but the stream is rtmpe
-##########   I am keeping the code in here so it doesn't get lost so when     
-#    vidlink="http://www.tnt.tv/video_cvp/cvp/videoData/?id=" + epID
-#    showLinks=XML.ElementFromURL(vidlink).xpath('//video/files/file[@type="hd"]')
-#    Log("++++++++++++++")
-#    clip=showLinks[0].text.replace("/tveverywhere","")
-#    vidname=clip.split("/")[-1]
-#    date1=clip.split("/")[-2]
-#    date2=date1.split("-")[-1]
-#    date3=date1.split("-")[-2]
-#    clip="/flash/" + date2 + "-" + date3 + "/" + vidname
-#    
-#    Log(clip)
-#    player="http://i.cdn.turner.com/xslo/cvp/player/cvp_1.3.6.24.swf?player=fw_main&domId=cvp_1&playerWidth=640&playerHeight=360"
-
-
-
-    dir.Append(WebVideoItem(clip, title=title, thumb=thumb, summary=summary))
-    Log("epID: " + epID + " | title: " + title + " | thumb: " + thumb + " | Description: " + summary)
-    
-  
+    thumb = show.xpath('./thumbnailUrl')[0].text
+    summary = show.xpath('./description')[0].text
+    #duration = show.xpath('./duration')          ###NEEDS WORK
+    #clip = "http://www.tnt.tv/dramavision/index.jsp?oid=" + videoId     ###SAVE THIS IN CASE RTMP BRAKES
+    #Log("link: " + clip)   
+    #dir.Append(WebVideoItem(clip, title=title, thumb=thumb, summary=summary))
+    #Log("epID: " + epID + " | title: " + title + " | thumb: " + thumb + " | Description: " + summary)
+    dir.Append(Function(VideoItem(VideoPlayer, title=title, thumb=thumb, summary=summary), videoId=videoId))
   return dir
+
+####################################################################################################
+def VideoPlayer(sender, videoId):
+    dir = MediaContainer(title2=sender.itemTitle)
+    postData = {}
+    streamInfo = "http://www.tnt.tv/video_cvp/cvp/videoData/?id=" + videoId
+    showLinks = XML.ElementFromURL(streamInfo)
+    clip = showLinks.xpath('//video/files/file[@type="hd"]')[0].text   ###ALL LINKS APPEAR TO BE THE SAME, NEED TO PREVENT ERROR IN CASE THERE IS NO "HD" ITEM
+    clip = clip.replace("/mp4:", "").replace(".mp4", "")
+    player = showLinks.xpath('//akamai/src')[0].text
+    player = "rtmp:" + player.split(":")[1]
+    postData['authTokenType'] = showLinks.xpath('//akamai/authTokenType')[0].text
+    postData['window'] = showLinks.xpath('//akamai/window')[0].text
+    postData['aifp'] = showLinks.xpath('//akamai/aifp')[0].text
+    postData['path'] = clip
+    postData['videoId'] = videoId
+    postData['profile'] = "tnt"
+    authString = XML.ElementFromURL(AUTH_URL, values=postData).xpath('token')[0].text
+    player = player + "?" + authString
+    clip = "mp4:" + clip
+    #Log(player)
+    #Log(clip)
+    return Redirect(RTMPVideoItem(player, clip))
+
